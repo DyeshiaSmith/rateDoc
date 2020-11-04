3dab59a341675db73bf4739980db097e3e7d6d51
D <dyeshiamonet@gmail.com>
2020-11-03 19:50:13 -0600

    add review doc table and js code for ratings

--- components/views/Reviewdoc.js
+++ components/views/Reviewdoc.js
@@ -1,5 +1,102 @@
 export default () => `
-<div>
-<p> A company that allows you to talk about you experiences blahhh Etiam egestas consectetur gravida. Nulla mollis suscipit sapien sed fermentum. Integer vitae eros a magna vulputate aliquam. Suspendisse sed pulvinar augue, auctor mollis lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eleifend diam quis libero sollicitudin efficitur. Nullam sapien eros, tempor eget vulputate ut, interdum vel orci.</p>
+<div class="review">
+  <div class="container">
+    <div class="form-group">
+    <select id="doctor-select" class="form-control custom-select">
+    <option value="0" disable selected>Select Category</option>
+    <option value="overall">Overall</option>
+    <option value="care">Caring</option>
+    <option value="patience">Patience</option>
+    <option value="listening">Listening</option>
+    <option value="knowledgeable">Knowledgeable</option>
+    <option value="environment">Environment</option>
+    <option value="timeliness">Timeliness</option>
+    </select>
+    </div>
+  <div class="form-group">
+    <input type="number" id="rating-control"
+    class="form-control" step="0.1" max="5" placeholder="Rate 1 - 5"
+  </div>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th>Category</th>
+        <th>Rating</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr class="overall">
+          <td>Overall</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+        <tr class="care">
+          <td>Caring</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+        <tr class="patience">
+          <td>Patience</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+        <tr class="listening">
+          <td>Willingness to Listen</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+        <tr class="knowledge">
+          <td>Knowledge of Treatment</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+        <tr class="environment">
+          <td>Environment</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+        <tr class="timeliness">
+          <td>Timeliness</td>
+          <td>
+          <div class="stars-outer">
+            <div class="stars-inner">
+            </div>
+            </div>
+            <span class="number-rating"></span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
 `;
--- index.js
+++ index.js
@@ -45,3 +45,70 @@ function addNavEventListeners() {
 document.querySelector(".card-btn").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
+
+// Leave reviews
+const ratings = {
+  overall: 4.7,
+  care: 3.4,
+  patience: 2.3,
+  listening: 3.6,
+  knowledgeable: 4.1,
+  environment: 4.5,
+  timeliness: 3.2
+};
+
+// Total Stars
+const starsTotal = 5;
+
+// Run getRatings when DOM loads
+document.addEventListener("DOMContentLoaded", getRatings);
+
+// Form=mss
+const doctorSelect = document.getElementById("doctor-select");
+const ratingControl = document.getElementById("rating-control");
+
+let doctor;
+
+//doc select change
+doctorSelect.addEventListener("change", e => {
+  doctor = e.target.value;
+  // Enable rating control
+  ratingControl.disabled = false;
+  ratingControl.value = ratings[doctor];
+});
+
+// Rating control change eventlistner.
+ratingControl.addEventListener("blur", e => {
+  const rating = e.target.value;
+
+  // Make sure 5 or under
+  if (rating > 5) {
+    alert("Please rate 1 to 5");
+    return;
+  }
+
+  // Change rating
+  ratings[doctor] = rating;
+
+  getRatings();
+});
+
+// Get ratings
+function getRatings() {
+  for (let rating in ratings) {
+    // Get percentage
+    const starPercentage = (ratings[rating] / starsTotal) * 100;
+
+    // Round to nearest 10
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
+    // Set width of stars-inner to percentage
+    document.querySelector(
+      `.${rating} .stars-inner`
+    ).style.width = starPercentageRounded;
+
+    // Add number rating
+    document.querySelector(`.${rating} .number-rating`).innerHTML =
+      ratings[rating];
+  }
+}
