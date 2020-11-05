e9e81f9d28736308d9cf5834a21a35e1e6f7d122
D <dyeshiamonet@gmail.com>
2020-11-05 16:55:00 -0600

    change logo in nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,6 @@
 export default links => `
 <nav>
-<img src="image/0b81c0ac-3bd1-4d9e-8b20-bd2e4ce1f79c_200x200.png">
+<img src="https://github.com/DyeshiaSmith/rateDoc/blob/master/pictures/0b81c0ac-3bd1-4d9e-8b20-bd2e4ce1f79c_200x200.png">
       <i class="fas fa-bars"></i>
       <ul class="hidden--mobile nav-links">
       ${links
